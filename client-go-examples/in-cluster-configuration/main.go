3f95140c9dd38477ceb5543a642d27de018cb340
hyj <huoyujia@sspaas.com>
2024-08-05 17:00:06 +0800

    feat:Add authentication configuration within the client go cluster

---
+++ client-go-examples/in-cluster-configuration/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	//1.初始化config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//2.通过config初始化clientset
+	clinetset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		//3.通过clientset来列出特定命名空间里的所有Pod
+		pods, err := clinetset.CoreV1().Pods("default").
+			List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		for i, pod := range pods.Items {
+			//4.打印信息
+			log.Printf("%d -> %s/%s", i+1, pod.Namespace, pod.Name)
+		}
+
+		<-time.Tick(5 * time.Second)
+	}
+}
